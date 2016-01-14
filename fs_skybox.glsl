bb9fef5bd5a9beb752111a5af831c2f48efd8fb5
Ben Quach <benquach16@yahoo.com>
2016-01-14 00:10:43 -0800

    create skybox shaders

---
+++ fs_skybox.glsl
@@ -0,0 +1,17 @@
+$input v_texcoord0
+
+/*
+ * Copyright 2011-2016 Branimir Karadzic. All rights reserved.
+ * License: https://github.com/bkaradzic/bgfx#license-bsd-2-clause
+ */
+
+#include "./common/common.sh"
+
+SAMPLERCUBE(skybox, 1);
+
+uniform mat4 u_mtx;
+
+void main()
+{
+
+}
