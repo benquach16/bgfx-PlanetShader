837818e4cf0065cb2576fc3f2cafa20b83251bf5
Ben Quach <benquach16@yahoo.com>
2016-01-15 02:06:58 -0800

    continue fine tuning planet shader

--- fs_planet.glsl
+++ fs_planet.glsl
@@ -4,11 +4,10 @@ $input v_pos, v_view, v_normal, v_color0, v_texcoord0
 #include "./common/common.sh"
 
 
-vec3 lightPosition = vec3(-5.0, 5.0, -5.0);
-
+vec3 lightPosition = vec3(-5.0, 4.0, 0.0);
 
 SAMPLER2D(s_planet_texture, 0);
-
+SAMPLER2D(s_planet_texture_day, 1);
 void main()
 {
 	vec3 normal = normalize(v_normal);
@@ -31,14 +30,31 @@ void main()
 	float rimLight = dot(lightDirection, normal);
 	float f_rim = pow(rim, 8.0);
 	f_rim *= (rimLight*2);
-
+	f_rim +=  0.5* rimLight * rim;
+	f_rim += 0.05;
 	//speculards
 	vec3 reflectVec = normalize(-reflect(lightDirection, normal));
 	float spec = 0.1* pow(max(dot(reflectVec, viewDirection), 0.0), 9.0);
-	vec4 tex = texture2D(s_planet_texture, v_texcoord0.xy/1.1);
-	tex = tex * max(dot(normal, lightDirection), 0.0);
+	vec4 tex = texture2D(s_planet_texture, v_texcoord0.xy);
+
+	//attenuation
+	float lightDistance = length(lightPosition);
+	float attenuation = 1.0;
 	
-	gl_FragColor = ambient + tex+ spec + f_rim * vec4(0.3,0.6,1.0,1.0);
-	//ogl_FragColor = texture2D(s_planet_texture, v_texcoord0.xy);
+    vec4 texDay = toLinearAccurate(texture2D(s_planet_texture_day, v_texcoord0.xy/1.1));
+	//vec4 texDay = texture2D(s_planet_texture_day, v_texcoord0.xy);
+	texDay = texDay * max(dot(normal, lightDirection), 0.0);
+	tex = tex * max(dot(normal, lightDirection), 0.0);
+	vec4 finalColor = texDay;
+	
+	gl_FragColor =ambient + attenuation*(finalColor+ spec) + f_rim * vec4(0.3,0.6,1.0,1.0);
+	
+
+	//gl_FragColor = toGammaAccurate(attenuation*(finalColor + spec));
+	//gl_FragColor = vec4(pow(attenuation*(finalColor.xyz + spec), vec3_splat(1.0/2.2)), 1.0);
+	//gl_FragColor += f_rim * vec4(0.3, 0.6, 1.0, 1.0);
 	gl_FragColor.w = 1.0;
+
+	//ogl_FragColor = texture2D(s_planet_texture, v_texcoord0.xy);
+
 }
