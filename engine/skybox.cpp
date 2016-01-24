a6d9307ce2b961e4f9364dcef570d5bcf93e2fd3
Ben Quach <benquach16@yahoo.com>
2016-01-24 13:24:35 -0800

    create skybox

--- engine/skybox.cpp
+++ engine/skybox.cpp
@@ -26,3 +26,51 @@ struct PosColorTexCoord0Vertex
 
 
 bgfx::VertexDecl PosColorTexCoord0Vertex::ms_decl;
+
+
+static PosColorTexCoord0Vertex s_cubeVertices[8] =
+{
+	{-1.0f,  1.0f,  1.0f, 0xff000000 },
+	{ 1.0f,  1.0f,  1.0f, 0xff0000ff },
+	{-1.0f, -1.0f,  1.0f, 0xff00ff00 },
+	{ 1.0f, -1.0f,  1.0f, 0xff00ffff },
+	{-1.0f,  1.0f, -1.0f, 0xffff0000 },
+	{ 1.0f,  1.0f, -1.0f, 0xffff00ff },
+	{-1.0f, -1.0f, -1.0f, 0xffffff00 },
+	{ 1.0f, -1.0f, -1.0f, 0xffffffff },
+};
+
+static const uint16_t s_cubeIndices[36] =
+{
+	0, 1, 2, // 0
+	1, 3, 2,
+	4, 6, 5, // 2
+	5, 6, 7,
+	0, 2, 4, // 4
+	4, 2, 6,
+	1, 5, 3, // 6
+	5, 7, 3,
+	0, 4, 1, // 8
+	4, 5, 1,
+	2, 3, 6, // 10
+	6, 3, 7,
+};
+
+void Skybox::setupSkybox()
+{
+	PosColorTexCoord0Vertex::init();
+	bgfx::VertexBufferHandle vbh = bgfx::createVertexBuffer(
+		// Static data can be passed with bgfx::makeRef
+		bgfx::makeRef(s_cubeVertices, sizeof(s_cubeVertices) )
+		, PosColorTexCoord0Vertex::ms_decl
+		);
+
+}
+
+
+void Skybox::renderSkybox(bgfx::ProgramHandle program)
+{
+
+
+	
+}
--- engine/skybox.h
+++ engine/skybox.h
@@ -2,14 +2,21 @@
 #define __SKYBOX_H__
 
 #include <bgfx/bgfx.h>
-
+#include <bgfx/bgfxplatform.h>
+#include <bgfx/bgfxdefines.h>
 
 //create a box here
 //lace camera inside it
 
 
+class Skybox
+{
+public:
+	void setupSkybox();
+	void renderSkybox(bgfx::ProgramHandle program);
 
 
+};
 
 
 #endif
